bc911d052c069d12eed825942d5ad455052f8adc
Jason Di <jasondisj@gmail.com>
2022-11-23 14:33:40 -0800

    Fix DelicateError package.json by creating client & server folder to differentiate frontend & backend

---
+++ client/components/RestaurantCard.js
@@ -0,0 +1,47 @@
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import React from "react";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { useNavigation } from "@react-navigation/native";
+
+const RestaurantCard = ({
+  id,
+  imgUrl,
+  title,
+  rating,
+  genre,
+  address,
+  shortDescription,
+  dishes,
+  longitude,
+  latitude,
+}) => {
+  const navigation = useNavigation;
+
+  return (
+    <TouchableOpacity className="bg-white mr-3 shadow">
+      <Image source={{ uri: imgUrl }} className="h-36 w-64 rounded-sm" />
+
+      <View className="px-3 pb-4">
+        <Text className="font-bold text-lg pt-2">{title}</Text>
+        <View className="flex-row items-center space-x-1">
+          <Ionicons name="star" color="green" opacity={0.5} size={22} />
+          <Text className="text-xs text-gray-500">
+            <Text className="text-green-500">{rating}</Text> · {genre}
+          </Text>
+        </View>
+
+        <View className="flex-row items-center space-x-1">
+          <Ionicons
+            name="location-outline"
+            color="gray"
+            opacity={0.4}
+            size={22}
+          />
+          <Text className="text-xs text-gray-500">Nearby · {address}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default RestaurantCard;
